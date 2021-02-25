e81338a7d58e4fd6bb411b8e80ac3d4ef4349cfc
dp <sodhox@mailbox.org>
2021-02-25 20:22:51 +0300

    task edit page refactored

--- resources/views/tasks-edit.php
+++ resources/views/tasks-edit.php
@@ -1,14 +1,13 @@
-<form action="<?php echo $dataArray[0]['formAction'] ?>" method="POST">
-
-    <?php
-
+<form action="<?= $dataArray[0]['formAction'] ?>" method="POST">
+    <?php 
         /* Вывод теущих значений полей */
-        $task = $dataArray[0]['task'];
-        foreach ($task as $fieldName => $value){
-            echo '<div class="mb-3">';
-            echo '<label for="input-'.$fieldName.'" class="form-label">'.$fieldName.'</label>';
-            echo '<input type="text" class="form-control" name="'.$fieldName.'" value="'.$value.'" id="input-login"><br>';
-            echo '<div>';
+        foreach ($dataArray[0]['task'] as $fieldName => $value){
+    ?>
+    <div class="mb-3">
+    <label for="input-<?=$fieldName?>" class="form-label"><?=$fieldName?></label>
+    <input type="text" class="form-control" name="<?=$fieldName?>" value="<?=$value?>" id="input-login"><br>
+    </div>
+    <?php 
         }
     ?>
     <button type="subimt" class="btn btn-primary">Отправить</button>
